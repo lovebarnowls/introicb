87c06d752b0913632fdd21a0191c21d4928848d5
lovebarnowls <114584264+lovebarnowls@users.noreply.github.com>
2023-01-26 16:39:43 +1000

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -13,6 +13,8 @@ sudo mkdir -p /home/ivan/desktop/iloveqld
 sudo mkdir -p /home/chris/desktop/ilovevic/vicisawesome
 sudo echo 'you should not have opened this' > /home/ivan/desktop/iloveqld/clickonthistomakeyourdreamscometrue.dll
 sudo echo 'you should not open this either' > /home/chris/desktop/ilovevic/vicisawesome/thisisabackdoor.exe
+sudo echo 'thisisforadvancedquestion1' > /home/chris/desktop/ilovevic/lullaby.txt
+sudo chown :1006 /home/chris/desktop/ilovevic/lullaby.txt
 sudo mkdir -p /home/james/desktop/
 sudo echo 'welcometocybertaipantraining' |base64 > /home/james/desktop/welcome.txt
 sudo apt install gedit john rkhunter sqlite3 mysql-server -y -qq
